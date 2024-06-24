c68bbfb99377917feaa4e2c7c6ca0932aaede40d
yinqizhenx <yinqizhen@aliyun.com>
2024-06-24 17:08:29 +0800

    update

--- adapter/handler/rpc/payment_center.go
+++ adapter/handler/rpc/payment_center.go
@@ -3,11 +3,14 @@ package rpc
 import (
 	"context"
 
+	"purchase/app/app_service"
 	pb "purchase/idl/payment_center"
 )
 
-type PaymentCenterHandler struct{}
+type PaymentCenterHandler struct {
+	srv *app_service.PaymentCenterAppService
+}
 
 func (h *PaymentCenterHandler) AddPaymentApply(ctx context.Context, req *pb.AddPAReq) (*pb.AddPARes, error) {
-	return nil, nil
+	return h.srv.AddPaymentApply(ctx, req)
 }
