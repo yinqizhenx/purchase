a98e3e01358593f2cb8f5f2c58470661de296e73
yinqizhenx <yinqizhen@aliyun.com>
2024-06-26 15:16:11 +0800

    update

--- domain/service/payment_center.go
+++ domain/service/payment_center.go
@@ -7,26 +7,27 @@ import (
 	"purchase/domain/entity/payment_center"
 	"purchase/domain/event"
 	"purchase/domain/repo"
+	"purchase/domain/sal"
 )
 
-// 在 domain.service 中定义领域服务的接口
-type SpamChecker interface {
-	Check(ctx context.Context, content string) error
-}
+// // 在 domain.service 中定义领域服务的接口
+// type SpamChecker interface {
+// 	Check(ctx context.Context, content string) error
+// }
 
 type PAService struct {
 	// repository.CommentRepository 和 ContentSal 在领域层都是以接口的形式存在
 	// 因为在领域层不关心具体的实现
-	repo        repo.PaymentCenterRepo
-	spamChecker SpamChecker
-	eventRepo   repo.EventRepo
+	repo      repo.PaymentCenterRepo
+	mdm       sal.MDMService
+	eventRepo repo.EventRepo
 }
 
-func NewPAService(repo repo.PaymentCenterRepo, checker SpamChecker, eventRepo repo.EventRepo) *PAService {
+func NewPAService(repo repo.PaymentCenterRepo, mdm sal.MDMService, eventRepo repo.EventRepo) *PAService {
 	return &PAService{
-		repo:        repo,
-		spamChecker: checker,
-		eventRepo:   eventRepo,
+		repo:      repo,
+		mdm:       mdm,
+		eventRepo: eventRepo,
 	}
 }
 
