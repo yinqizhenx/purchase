5130bc708f813b15c928b8aa272ca67316db6e27
yinqizhenx <yinqizhen@aliyun.com>
2024-07-18 16:58:25 +0800

    udpate

--- infra/mq/kafka/pub.go
+++ infra/mq/kafka/pub.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/go-kratos/kratos/v2/config"
 	"github.com/google/wire"
@@ -20,29 +21,35 @@ var ProviderSet = wire.NewSet(NewKafkaPublisher, NewKafkaSubscriber)
 
 type kafkaPublisher struct {
 	writer *kafka.Writer
-	// topic  string
-	idg mq.IDGenFunc
+	idg    mq.IDGenFunc
 }
 
 func (s *kafkaPublisher) Publish(ctx context.Context, msg *mq.Message) error {
-	kMsg := &kafka.Message{
+	kmsg, err := s.buildKafkaMessage(msg)
+	if err != nil {
+		return err
+	}
+	return s.writer.WriteMessages(ctx, *kmsg)
+}
+
+func (s *kafkaPublisher) buildKafkaMessage(msg *mq.Message) (*kafka.Message, error) {
+	kmsg := &kafka.Message{
 		Key:   []byte(msg.HeaderGet(mq.EntityID)),
 		Value: msg.Body,
+		Topic: msg.HeaderGet(mq.EventName),
 	}
-	m := NewMessage(kMsg)
-	m.SetEventName(msg.HeaderGet(mq.EventName))
-	m.SetMessageID(s.idg())
 
-	kMsg, err := m.ToKafkaMessage()
-	kMsg.Topic = msg.HeaderGet(mq.EventName)
-	if err != nil {
-		return err
+	for k, v := range msg.Header() {
+		err := SetMessageHeader(kmsg, k, v)
+		if err != nil {
+			return nil, err
+		}
 	}
-	err = s.writer.WriteMessages(ctx, *kMsg)
+	err := SetMessageHeader(kmsg, mq.MessageID, s.idg())
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return kmsg, nil
 }
 
 func (s *kafkaPublisher) Close() error {
@@ -55,14 +62,41 @@ func NewKafkaPublisher(c config.Config, idg mq.IDGenFunc) (mq.Publisher, error) 
 	if err != nil {
 		return nil, err
 	}
-	// topic, err := c.Value("kafka.topic").String()
-	// if err != nil {
-	// 	return nil, err
-	// }
 	w := &kafka.Writer{
-		// Topic:    topic,
 		Addr:     kafka.TCP(address...),
 		Balancer: &kafka.LeastBytes{},
 	}
 	return &kafkaPublisher{writer: w, idg: idg}, nil
+}
+
+func SetMessageHeader(m *kafka.Message, k string, v interface{}) error {
+	val, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	// 存在重复key，直接覆盖
+	for i, header := range m.Headers {
+		if header.Key == k {
+			m.Headers[i] = kafka.Header{
+				Key:   k,
+				Value: val,
+			}
+			return nil
+		}
+	}
+	m.Headers = append(m.Headers, kafka.Header{
+		Key:   k,
+		Value: val,
+	})
+	return nil
+}
+
+// GetMessageHeader 获取消息header值， val需为指针
+func GetMessageHeader(m kafka.Message, k string, val interface{}) error {
+	for _, header := range m.Headers {
+		if header.Key == k {
+			return json.Unmarshal(header.Value, val)
+		}
+	}
+	return nil
 }
